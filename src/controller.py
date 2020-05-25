509ff8650336ee7c20cfdfe42e17d074e0bd4f69
christopher simak <chris@christophers-MacBook-Air.local>
2020-05-24 22:47:43 -0400

    start up

---
+++ src/controller.py
@@ -0,0 +1,11 @@
+import sys
+import pygame
+import random
+from src import bearcat
+from src import bear
+from src import gator
+from src import gamecock
+
+class Controller:
+    def __init__(self, width=640, height=480):
+        self.screen = 
