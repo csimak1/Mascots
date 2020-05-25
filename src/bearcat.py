bdb7dc2af4e4b9077e68ee09016774cf539c7639
Patrick Lyons <patricklyons@Patricks-MBP.lan>
2020-05-25 01:33:27 -0400

    methods

--- src/bearcat.py
+++ src/bearcat.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
 class Bearcat(pygame.sprite.Sprite):
     def __init__(self):
-    self.health = 100
-    self.hit = 15
-    self.image = pygame.image.load("assets/binghamton.png")
-    
+        self.name = "Binghamton"
+        self.person = "Zac"
+        self.health = random.randint(1,10)
+        self.image = pygame.image.load("assets/binghamton.png")
